24a1d314b81c36902e223d13cd8f955379c3c312
sanjana <sanjanarathnayake692@gmail.com>
2026-02-03 21:15:04 +0530

    edit fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export const metadata: Metadata = {
   description:
     "Revolutionary technology solutions that drive growth and innovation",
   generator: "v0.app",
+  icons: {
+    icon: "/images/newlogo.png",
+  },
 };
 
 export default function RootLayout({
